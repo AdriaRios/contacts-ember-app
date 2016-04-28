4dd6275a19e6f107dc43632dfc6dc647bef1b2b5
Adrian  Rios Cabrera <adrianrioscabrera@MacBook-Air-de-Kerad-4.local>
2016-04-28 15:27:22 +0200

    Show Numbers

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,8 +1,11 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
+import { hasMany } from 'ember-data/relationships';
+
 
 export default Model.extend({
   nameFirst: attr(),
   nameLast: attr(),
-  email: attr()
+  email: attr(),
+  phoneNumbers: hasMany('phoneNumber')
 });
--- app/routes/contacts-list/contact.js
+++ app/routes/contacts-list/contact.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import { hasMany } from 'ember-data/relationships';
 
 export default Ember.Route.extend({
   model(params) {
