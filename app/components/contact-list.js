90d0647095bfda79ded074cf14019f1c5c1da53b
Adrian  Rios Cabrera <adrianrioscabrera@MacBook-Air-de-Kerad-4.local>
2016-04-28 09:54:52 +0200

    (FIX) Contact-list is now a component

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //Actions
+  actionClick: 'click',
+  actionAddContact: 'addContact',
+  //Actions Handlers
+  actions: {
+    click(params) {
+      this.sendAction('actionClick', params);
+    },
+    addContact() {
+      this.sendAction('actionAddContact');
+    }
+  }
 });
--- app/routes/contacts-list.js
+++ app/routes/contacts-list.js
@@ -7,9 +7,11 @@ export default Ember.Route.extend({
   },
   actions: {
     click(params) {
+      console.log('TEST');
       this.transitionTo('contacts-list.contact', params.id);
     },
     addContact() {
+      console.log('TEST2');
       this.transitionTo('contacts-list.add-contact');
     }
   }
